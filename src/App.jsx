117fe40d6b918628d566af8c2ef2efca8ec646ae
Alantitus <alantitus89@gmail.com>
2024-04-15 01:55:33 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import './App.css'; 
+
+function App() {
+  const [backgroundColor, setBackgroundColor] = useState('#ffffff');
+
+  const changeBackgroundColor = (color) => {
+    document.body.style.backgroundColor = color;
+    setBackgroundColor(color);
+  };
+
+  return (
+    <div className="App">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg" alt=""  width={"100px"} className='m-5'/>
+      <h1> Change Background Color</h1>
+      <div style={{ backgroundColor: backgroundColor }} className="color-box m-5"></div>
+      <button onClick={() => changeBackgroundColor('#ff6500')}>Orange</button>
+      <button onClick={() => changeBackgroundColor('#0077B6')}> Blue</button>
+      <button onClick={() => changeBackgroundColor('#32cd32')}>Green</button>
+      <button onClick={() => changeBackgroundColor('#ff0000')}>Red</button>
+      
+    </div>
+  );
+}
+
+export default App;
