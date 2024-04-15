6cd28d3fb9019862a17fae6c45cd4c670858c578
Alantitus <alantitus89@gmail.com>
2024-04-15 11:50:22 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -12,12 +12,13 @@ function App() {
   return (
     <div className="App">
       <img src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg" alt=""  width={"100px"} className='m-5'/>
-      <h1> Change Background Color</h1>
+      <h2>Click to change background</h2>
       <div style={{ backgroundColor: backgroundColor }} className="color-box m-5"></div>
       <button onClick={() => changeBackgroundColor('#ff6500')}>Orange</button>
       <button onClick={() => changeBackgroundColor('#0077B6')}> Blue</button>
       <button onClick={() => changeBackgroundColor('#32cd32')}>Green</button>
       <button onClick={() => changeBackgroundColor('#ff0000')}>Red</button>
+      <button onClick={() => changeBackgroundColor('#ffff00')}>Yellow</button>
       
     </div>
   );
